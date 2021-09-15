f97b91cf570df5734127ae5ee6ae5392d9ba9ff3
RacoonDog <32882447+RacoonDog@users.noreply.github.com>
2021-09-14 23:32:47 -0400

    updated dependencies

--- src/main/java/io/github/racoondog/colorimetry/util/ARRPUtil.java
+++ src/main/java/io/github/racoondog/colorimetry/util/ARRPUtil.java
@@ -11,11 +11,11 @@ public class ARRPUtil {
     public static void addLootTable(Identifier id, RuntimeResourcePack RRP) {
         RRP.addLootTable(StringUtil.prefixPath(id, "blocks/"), loot("minecraft:block").pool(pool().rolls(1).entry(entry().type("minecraft:item").name(id.toString())).condition(predicate("minecraft:survives_explosion"))));
     }
-    public static void addBlockState(Identifier id, String template, RuntimeResourcePack RRP) {
-        RRP.addBlockState(state(variant(model(new StringBuilder(id.getNamespace()).append(":block/").append(template.toLowerCase()).toString()))), id);
+    public static void addBlockState(Identifier id, String parent, RuntimeResourcePack RRP) {
+        RRP.addBlockState(state(variant(model(new StringBuilder(id.getNamespace()).append(":block/").append(parent.toLowerCase()).toString()))), id);
     }
-    public static void addBlockItemModel(Identifier id, String template, RuntimeResourcePack RRP) {
-        RRP.addModel(JModel.model(new StringBuilder(id.getNamespace()).append(":block/").append(template.toLowerCase()).toString()), StringUtil.prefixPath(id, "item/"));
+    public static void addBlockItemModel(Identifier id, String parent, RuntimeResourcePack RRP) {
+        RRP.addModel(JModel.model(new StringBuilder(id.getNamespace()).append(":block/").append(parent.toLowerCase()).toString()), StringUtil.prefixPath(id, "item/"));
     }
     public static void addBlockItemModel(Identifier id, RuntimeResourcePack RRP) {
         addBlockItemModel(id, id.getPath(), RRP);
